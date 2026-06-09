d32842cdce93d133d48dfba13f0bb7b56f14bb16
deandoff <deandoff@gmail.com>
2026-06-09 20:22:46 +0300

    Fix multipart file uploads

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -4,9 +4,6 @@ const API_URL = process.env.REACT_APP_API_URL || '';
 
 const api = axios.create({
   baseURL: API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
 export interface UploadTarget {
